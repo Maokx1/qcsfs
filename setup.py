7a890d364692350eb2e2fc1b560037b3ad1b08a3
Maokx1 <4mprog@gmail.com>
2023-03-19 21:26:46 +0100

    Added setup files and requirements

---
+++ setup.py
@@ -0,0 +1,6 @@
+# setup.py is deprecated (PEP 621) (https://peps.python.org/pep-0621/)
+# However I use it if someone wants to use editable installs.
+# https://stackoverflow.com/questions/35064426/when-would-the-e-editable-option-be-useful-with-pip-install
+from setuptools import setup
+
+setup()
